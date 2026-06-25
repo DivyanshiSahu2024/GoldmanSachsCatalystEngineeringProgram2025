3ccd404d7216e39fa2d4d537319ef59beb6a97ae
Divyanshi Sahu <sahudiv0309@gmail.com>
2026-06-25 11:21:04 +0530

    Update and rename bubble-sort to bubble-sort.java

--- Sorting/bubble-sort.java
+++ Sorting/bubble-sort.java
@@ -3,13 +3,13 @@ class Solution
 {
 	void bubbleSort(int arr[]) {
 		int n = arr.length;
-// Traverse through all array elements
+		// Traverse through all array elements
 		for (int i = 0; i < n - 1; i++) {
-// Last i elements are already in place
+		// Last i elements are already in place
 			for (int j = 0; j < n - i - 1; j++) {
-// Swap if the element found is greater than the next element
+			// Swap if the element found is greater than the next element
 				if (arr[j] > arr[j + 1]) {
-// swap arr[j] and arr[j+1]
+					// swap arr[j] and arr[j+1]
 					int temp = arr[j];
 					arr[j] = arr[j + 1];
 					arr[j + 1] = temp;
@@ -17,7 +17,7 @@ class Solution
 			}
 		}
 	}
-// Utility method to print the array
+	// Utility method to print the array
 	void printArray(int arr[]) {
 		for (int value : arr) {
 			System.out.print(value + " ");
