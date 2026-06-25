a928dec0a1c03a0604731311680c1e6a70dbb298
Divyanshi Sahu <sahudiv0309@gmail.com>
2026-06-25 11:23:31 +0530

    Update and rename selection sort to selection-sort.java

--- Sorting/selection-sort.java
+++ Sorting/selection-sort.java
@@ -1,16 +1,17 @@
 //  Problem:  https://practice.geeksforgeeks.org/problems/selection-sort/1
+
 class Solution {
 	void selectionSort(int arr[]) {
 		int n = arr.length;
 		for (int i = 0; i < n - 1; i++) {
-// Find the minimum element in unsorted array
+			// Find the minimum element in unsorted array
 			int minIndex = i;
 			for (int j = i + 1; j < n; j++) {
 				if (arr[j] < arr[minIndex]) {
 					minIndex = j;
 				}
 			}
-// Swap the found minimum element with the first element
+			// Swap the found minimum element with the first element
 			int temp = arr[minIndex];
 			arr[minIndex] = arr[i];
 			arr[i] = temp;
